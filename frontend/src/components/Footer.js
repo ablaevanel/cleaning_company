4aea3576efe4bb5e674339a2951090b755ed76c8
Anel' Ablaeva <90471578+ablaevanel@users.noreply.github.com>
2025-05-05 21:27:29 +0300

    Add files via upload

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,9 @@
+import '../styles/Footer.css';
+
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <p>© 2025 Служба уборки "Чистый Дом"</p>
+        </footer>
+    );
+}
