c025a3977345b35fd99e16a3909a3bc7323cde56
Anel' Ablaeva <90471578+ablaevanel@users.noreply.github.com>
2025-05-05 21:22:12 +0300

    Create db.go

---
+++ backend/db/db.go
@@ -0,0 +1,40 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
+)
+
+var DB *pgxpool.Pool
+
+func Connect() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Ошибка загрузки .env файла")
+	}
+
+	dsn := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s?sslmode=disable",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	pool, err := pgxpool.New(ctx, dsn)
+	if err != nil {
+		log.Fatal("Ошибка подключения к базе:", err)
+	}
+
+	DB = pool
+}
